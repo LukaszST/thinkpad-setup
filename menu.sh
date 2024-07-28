29cffe97bb9398a1a7a043b738dbc79f4b973464
Lukasz Urbaniak <lukasz.i.urbaniak@gmail.com>
2024-07-28 21:57:27 +0200

    Add docker with docker.io and docker-ce approach

--- menu.sh
+++ menu.sh
@@ -32,6 +32,20 @@ function tlp_install() {
     sleep 2
 }
 
+function docker_docs_install() {
+    source $PWD/installers/${distro}/docker-docs.sh
+    echo "Docker installed"
+#    For now I will use sleep to not showing menu after message from installer
+    sleep 2
+}
+
+function docker_omakub_install() {
+    source $PWD/installers/${distro}/docker-omakub.sh
+    echo "Docker installed"
+#    For now I will use sleep to not showing menu after message from installer
+    sleep 2
+}
+
 function complete_install() {
     tlp_install
 }
@@ -56,12 +70,16 @@ menu() {
 echo -ne "
 Select option to install
 $(ColorGreen '1)') Install tlp and set charge limit
+$(ColorGreen '2)') Install Docker using docker-ce
+$(ColorGreen '3)') Install Docker using docker.io
 $(ColorGreen '5)') Install everything
 $(ColorGreen '0)') Exit
 $(ColorBlue 'Choose an option:') "
         read a
         case $a in
 	        1) tlp_install ; menu ;;
+            2) docker_docs_install ; menu ;;
+            3) docker_omakub_install ; menu ;;
 	        5) complete_install ; menu ;;
 		0) exit 0 ;;
 		*) echo -e $red"Wrong option."$clear; WrongCommand;;
