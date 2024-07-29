c1a179647bb9c64a76962ef22b455a0377ecf9bc
Lukasz Urbaniak <lukasz.i.urbaniak@gmail.com>
2024-07-29 23:10:00 +0200

    Add zsh with ohmyzsh

---
+++ installers/pop/zsh.sh
@@ -0,0 +1,16 @@
+ZSH_FILE=~/.zshrc
+ACTIVATE_LINE_STRING='eval "$(/usr/bin/mise activate zsh)"'
+ALIAS_FOR_VIM='alias vim=nvim'
+sudo apt install -y build-essential curl file git
+sudo apt install -y zsh
+chsh -s $(which zsh)
+sudo apt install -y git-core fonts-powerline
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+if ! grep -qF "$ACTIVATE_LINE_STRING" "$ZSH_FILE"; then
+  echo "$ACTIVATE_LINE_STRING" >>"$ZSH_FILE"
+fi
+
+if ! grep -qF "$ALIAS_FOR_VIM" "$ZSH_FILE"; then
+  echo "$ALIAS_FOR_VIM" >>"$ZSH_FILE"
+fi
